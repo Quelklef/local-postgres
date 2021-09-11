530cc1496a19824fd41d8055a503be67e67c93b7
Maynard <elimaynard923@gmail.com>
2021-09-11 15:38:01 -0500

    add "fail" func in tests

--- test.sh
+++ test.sh
@@ -17,23 +17,28 @@ testing() {
   echo
 }
 
+fail() {
+  echo "TEST FAILED: ${1:-test failed}"
+  exit 1
+}
+
 # ---------------------------------------------------------------------------- #
 testing "create, start, query"
 lpg make ./testdir/pg
 lpg do ./testdir/pg pg_ctl start
 result=$(lpg do ./testdir/pg psql -U postgres -tc 'SELECT 1, 2;')
 echo "result: $result"
-[ "$result" = '        1 |        2' ] || exit 1
+[ "$result" = '        1 |        2' ] || fail
 
 # ---------------------------------------------------------------------------- #
 testing "sandbox, query"
 reuslt=$(echo $'pg_ctl start && psql -U postgres -tc "SELECT 1, 2;"' | lpg shell --sandbox)
 echo "result: $result"
-[ "$result" = '        1 |        2' ] || exit 1
+[ "$result" = '        1 |        2' ] || fail
 
 # ---------------------------------------------------------------------------- #
 testing "sandbox: cleans up temp dir?"
 loc=$(echo 'echo "$LPG_LOC"' | lpg shell --sandbox)
 echo "loc: $loc"
-[ -n "$loc" ] || exit 1
-[ ! -e "$loc" ] || exit 1
+[ -n "$loc" ] || fail "LPG_LOC not printed"
+[ ! -e "$loc" ] || fail "temp dir not cleaned up"
