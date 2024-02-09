4ae7c5b771d0bcfdf9d486daaf254b48ef046591
Maynard <me@maynards.site>
2024-02-09 14:32:14 -0800

    change existing cli and add new commands and options

--- default.nix
+++ default.nix
@@ -8,35 +8,27 @@ script = ''
 #!${pkgs.bash}/bin/bash
 
 function lpg-help {
-  cat <<EOF
+  cat <<'EOF'
 lpg (Local PostGres): manage local PostgreSQL instances
-
 
 Basic Commands:
 
   lpg make <loc>
-
       Create an lpg-managed PostgreSQL instance at the specified location.
       The instance will be initialized with a superuser named 'postgres'
       Ex: lpg make ./pg
 
-  lpg shell (<loc> | --sandbox) [--auto | -a]
-
-      Enter an interactive shell with a modified environment such that libpq
+  lpg on <loc> enter [--auto | -a]
+      Enter an interactive bash shell with a modified environment such that libpq
       commands, like psql and pg_ctl, will use the lpg instance at <loc>.
 
-      If '--sandbox' is given, use a temporary anonymous lpg instance instead.
-      The instance will be stopped when the shell exits.
-
-      If '--auto' is given, start the instance when the shell is
+      If --auto is given, start the instance when the shell is
       entered (unless it is already running), and stop the instance when the
       shell is exited (unless it has already stopped).
 
       Environment modifications are:
         - LPG_IN_SHELL is set to '1'
         - LPG_LOC is set to an absolute version of <loc>
-        - LPG_CONNSTR is set to a PostgreSQL connection string for the
-          given lpg instance
         - PGDATA and PGHOST are set
         - pg_ctl is monkeypatched to:
             - log to <loc>/log
@@ -46,49 +38,51 @@ Basic Commands:
             - log in with user postgres by default instead of $USER
           Note that this behaviour can be overturned by passing your
           own CLI arguments, e.g. 'psql -U $USER'
+        - A bash function lpg-get-connstr() is defined which produces
+          a PostgreSQL connection string for the lpg instance.
+          The function signature matches 'lpg on <loc> get-connstr';
+          see below.
 
-  lpg env (<loc> | --sandbox) [--auto | -a]
-
-      Like 'lpg shell', but instead of entering an interactive shell, prints
-      a sourceable bash script.
-      Ex: source <(lpg env --sandbox) && pg_ctl start
+  lpg on <loc> env [--auto | -a]
+      Like 'lpg on <loc> enter', but instead of entering an interactive shell,
+      prints a sourceable bash script.
+      Ex: source <(lpg env ./pg) && pg_ctl start
 
   lpg help
-
       Show this message
 
+Convenience Commands:
 
-Derived Commands:
+  lpg on <loc> cmd <cmd>...
+      Run a command on an lpg instance
+      Ex: lpg on ./pg cmd psql -U postgres -tc 'SELECT * FROM mytable;'
 
-  Convenience commands built on top of the basic commands
-
-  lpg cmd <loc> <cmd>...
-      Run a command on an lpg instance without affecting the shell
-      Ex: lpg cmd ./pg psql -U postgres -tc 'SELECT * FROM mytable;'
-
-  lpg bash <loc> <str>
+  lpg on <loc> run <str>
       Run a bash command on an lpg instance
-      Ex: lpg bash ./pg 'pg_ctl stop && pg_ctl start'
+      Ex: lpg on ./pg run 'pg_ctl stop && pg_ctl start'
 
-  lpg pg-start <loc>
-      Start an lpg instance.
-      Equivalent to: lpg cmd <loc> pg_ctl start
+  lpg on <loc> psql
+      Equivalent to: lpg on <loc> run psql
 
-  lpg pg-stop <loc>
-      Stop an lpg instance.
-      Equivalent to: lpg cmd <loc> pg_ctl stop
+  lpg on <loc> get-connstr [-d <target-database>]
+      Prints the PostgreSQL connection string for an lpg instance,
+      optionally targeting a database other than the default database.
 
-  lpg pg-restart <loc>
-      Restart an lpg instance.
-      Equivalent to: lpg bash <loc> 'pg_ctl stop && pg_ctl start'
-
-  lpg pg-up <loc>
+  lpg on <loc> up
       Start an lpg instance if it is not already running
-      Equivalent to: lpg bash <loc> 'pg_ctl status || pg_ctl start'
+      Equivalent to: lpg on <loc> run 'pg_ctl status || pg_ctl start'
 
-  lpg pg-down <loc>
+  lpg on <loc> down
       Stop an lpg instance if it is running
-      Equivalent to: lpg bash <loc> 'if pg_ctl status; then pg_ctl stop; else true; fi'
+      Equivalent to: lpg on <loc> run 'if pg_ctl status; then pg_ctl stop; else true; fi'
+
+  lpg on <loc> restart
+      Run 'down' and then 'up'
+
+  lpg sandbox
+      Create an anonymous temporary lpg instance and enter into
+      its shell, automatically starting the postgres database
+      and stopping it when the shell is exited.
 
 EOF
 }
@@ -105,21 +99,12 @@ function lpg-make {
 
 function lpg-env {
   [[ $# -ge 1 ]] || { echo >&2 "Expected at least 1 argument"; return 1; }
-  [[ $# -le 2 ]] || { echo >&2 "Expected at most 2 arguments"; return 1; }
 
-  do_sandbox=
-  [ "$1" = --sandbox ] && do_sandbox=1 || do_sandbox=0
+  [[ -d "$1" ]] || { echo >&2 "$1 does not exist"; return 1; }
+  local dir=$(realpath "$1")
 
   do_auto=
   [ "$2" = '--auto' -o "$2" = '-a' ] && do_auto=1 || do_auto=0
-
-  if (( $do_sandbox )); then
-    local dir=$(mktemp -du)
-    lpg-make "$dir" >/dev/null || return 1
-  else
-    [[ -d "$1" ]] || { echo >&2 "$1 does not exist"; return 1; }
-    local dir=$(realpath "$1")
-  fi
 
   cat <<EOF
 
@@ -128,7 +113,6 @@ export PGHOST=$dir/socket
 
 export LPG_IN_SHELL=1
 export LPG_LOC=$dir
-export LPG_CONNSTR=postgresql://postgres@localhost?host=$dir/socket
 
 function pg_ctl {
   ${postgresql}/bin/pg_ctl \\
@@ -146,27 +130,27 @@ function psql {
 }
 export -f psql
 
+function lpg-get-connstr {
+  local db=
+  case "\$1" in
+    -d) shift; db="\$1"; shift ;;
+    "") ;;
+    *) echo >&2 "Bad call to lpg-get-connstr. Called like: lpg-get-connstr \$@"; return 1 ;;
+  esac
+  echo "postgresql://postgres@localhost/\$db?host=$dir/socket"
+}
+
 EOF
 
   (( $do_auto )) && cat <<EOF
 pg_ctl status >/dev/null || pg_ctl start
-EOF
-
-  # Remark: bash 'trap' overrides previous 'trap' calls
-
-  (( $do_sandbox )) && cat <<EOF
-lpg-cleanup() { pg_ctl status >/dev/null && pg_ctl stop; rm -rf "$dir"; }
-trap lpg-cleanup EXIT INT TERM
-EOF
-
-  (( $do_auto && !$do_sandbox )) && cat <<EOF
 lpg-cleanup() { pg_ctl status >/dev/null && pg_ctl stop; }
 trap lpg-cleanup EXIT INT TERM
 EOF
 
 }
 
-function lpg-shell {
+function lpg-enter {
   ( source <(lpg-env "$@") && bash )
 }
 
@@ -178,7 +162,7 @@ function lpg-cmd {
   ( source <(lpg-env "$dir") && "$@" )
 }
 
-function lpg-bash {
+function lpg-run {
   [[ $# -eq 2 ]] || { echo >&2 "Expected exactly 2 arguments"; return 1; }
   [[ -d "$1" ]] || { echo >&2 "$1 does not exist or is not a directory."; return 1; }
   local dir=$1; shift;
@@ -187,44 +171,46 @@ function lpg-bash {
   ( source <(lpg-env "$dir") && bash -c "$str" )
 }
 
-function lpg-pg-smth {
-  local str=$1; shift;
-  [[ $# -eq 1 ]] || { echo >&2 "Expected exactly 1 argument"; return 1; }
-  [[ -d "$1" ]] || { echo >&2 "$1 does not exist or is not a directory."; return 1; }
-  local dir=$1; shift;
-
-  ( source <(lpg-env "$dir") && bash -c "$str" )
-}
-
-function lpg-pg-start   { lpg-pg-smth 'pg_ctl start'   "$@"; }
-function lpg-pg-stop    { lpg-pg-smth 'pg_ctl stop'    "$@"; }
-function lpg-pg-restart { lpg-pg-smth 'pg_ctl restart' "$@"; }
-function lpg-pg-up      { lpg-pg-smth 'pg_ctl status || pg_ctl start' "$@"; }
-function lpg-pg-down    { lpg-pg-smth 'if pg_ctl status; then pg_ctl stop; else true; fi' "$@"; }
-
 
 function _main {
+
+  # Validate
+  case "$1" in
+    make|on|help|sandbox ) ;;
+    * ) lpg-help; exit 0 ;;
+  esac
+
+  # Rewrite 'lpg on <loc> <cmd> <args>...' to 'lpg <cmd> <loc> <args>...'
+  if [ "$1" = 'on' ]; then
+    shift
+    loc="$1"; shift
+    cmd="$1"; shift
+    set -- "$cmd" "$loc" "$@"
+  fi
+
   local cmd_name="$1"; shift;
 
   case "$cmd_name" in
     # Basic commands
     make       ) lpg-make "$@" ;;
     env        ) lpg-env "$@" ;;
-    shell      ) lpg-shell "$@" ;;
+    enter      ) lpg-enter "$@" ;;
 
     # Derived commands
-    cmd        ) lpg-cmd "$@" ;;
-    bash       ) lpg-bash "$@" ;;
-    pg-start   ) lpg-pg-start "$@" ;;
-    pg-stop    ) lpg-pg-stop "$@" ;;
-    pg-restart ) lpg-pg-restart "$@" ;;
-    pg-up      ) lpg-pg-up "$@" ;;
-    pg-down    ) lpg-pg-down "$@" ;;
+    cmd         ) lpg-cmd "$@" ;;
+    run         ) lpg-run "$@" ;;
+    psql        ) local dir="$1"; shift; lpg-cmd "$dir" psql "$@" ;;
+    get-connstr ) local dir="$1"; shift; lpg-cmd "$dir" lpg-get-connstr "$@" ;;
+    up          ) local dir="$1"; shift; lpg-run "$dir" 'pg_ctl status || pg_ctl start' ;;
+    down        ) local dir="$1"; shift; lpg-run "$dir" 'if pg_ctl status; then pg_ctl stop; else true; fi' ;;
+    restart     ) local dir="$1"; shift; lpg-run "$dir" 'if pg_ctl status; then pg_ctl stop; else true; fi; pg_ctl start' ;;
+    sandbox     ) local dir=$(mktemp -du) && lpg-make "$dir" && lpg-enter "$dir" --auto && rm -rf "$dir" ;;
 
     # Help
-    help       ) lpg-help "$@" ;;
+    help       ) lpg-help ;;
     *          ) lpg-help ;;
   esac
+
 }
 
 _main "$@"
--- test.sh
+++ test.sh
@@ -13,6 +13,7 @@ testing() {
   rm -rf ./testdir
   mkdir ./testdir
   echo
+  echo
   echo "[[ ============ Testing: $1 ============ ]]"
   echo
 }
@@ -22,27 +23,32 @@ fail() {
   exit 1
 }
 
+run() {
+  echo "RUNNING: $@"
+  "$@"
+}
+
 export LC_ALL=C.UTF-8  # pg needs it for some reason
 
 # ---------------------------------------------------------------------------- #
 testing "create, start, query"
-lpg make ./testdir/pg
-lpg cmd ./testdir/pg pg_ctl start
-result=$(lpg cmd ./testdir/pg psql -tc 'SELECT 1, 2;')
+run lpg make ./testdir/pg
+run lpg on ./testdir/pg cmd pg_ctl start
+result=$(lpg on ./testdir/pg psql -tc 'SELECT 1, 2;')
 echo "result: 〈$result〉"
 [ "$result" = '        1 |        2' ] || fail
 
 # ---------------------------------------------------------------------------- #
 testing "create, query, auto"
-lpg make ./testdir/pg
-result=$(echo 'psql -tc "SELECT 1, 2;"' | lpg shell ./testdir/pg --auto)
+run lpg make ./testdir/pg
+result=$(echo 'psql -tc "SELECT 1, 2;"' | lpg on ./testdir/pg enter --auto)
 echo "result: 〈$result〉"
 [[ $result == *'        1 |        2'* ]] || fail
 
 # ---------------------------------------------------------------------------- #
 testing "create, query, auto: cleans up pg process?"
 lpg make ./testdir/pg
-out=$(echo 'echo "$LPG_LOC" && pg_ctl status' | lpg shell ./testdir/pg --auto)
+out=$(echo 'echo "$LPG_LOC" && pg_ctl status' | lpg on ./testdir/pg enter --auto)
 echo "$out"
 pg_pid=$(echo "$out" | grep -oP '(?<=PID: )[0-9]+')
 echo "pg pid: $pg_pid"
@@ -51,20 +57,20 @@ kill -0 "$pg_pid" 2>/dev/null && is_running=1 || is_running=0
 
 # ---------------------------------------------------------------------------- #
 testing "sandbox, auto, query"
-result=$(echo $'psql -tc "SELECT 1, 2;"' | lpg shell --sandbox -a)
+result=$(echo $'psql -tc "SELECT 1, 2;"' | lpg sandbox)
 echo "result: 〈$result〉"
 [[ $result == *'        1 |        2'* ]] || fail
 
 # ---------------------------------------------------------------------------- #
 testing "sandbox: cleans up temp dir?"
-loc=$(echo 'echo "$LPG_LOC"' | lpg shell --sandbox)
+loc=$(echo 'echo "$LPG_LOC"' | lpg sandbox)
 echo "loc: $loc"
 [ -n "$loc" ] || fail "LPG_LOC not printed"
 [ ! -e "$loc" ] || fail "temp dir not cleaned up"
 
 # ---------------------------------------------------------------------------- #
 testing "sandbox: cleans up pg process?"
-out=$(echo 'pg_ctl start && echo "$LPG_LOC" && pg_ctl status' | lpg shell --sandbox)
+out=$(echo 'echo "$LPG_LOC" && pg_ctl status' | lpg sandbox)
 echo "$out"
 pg_pid=$(echo "$out" | grep -oP '(?<=PID: )[0-9]+')
 echo "pg pid: $pg_pid"
@@ -74,22 +80,24 @@ kill -0 "$pg_pid" 2>/dev/null && is_running=1 || is_running=0
 # ---------------------------------------------------------------------------- #
 testing "convenience commands"
 echo '~~ make'
-lpg make ./testdir/pg ; echo "$?"
-echo '~~ start'
-lpg pg-start ./testdir/pg ; echo "$?"
+lpg make ./testdir/pg ; [ "$?" = 0 ] || fail
+echo '~~ get-connstr'
+result=$(lpg on ./testdir/pg get-connstr -d my-db) ; [ "$?" = 0 ] || fail
+[ "$result" = "postgresql://postgres@localhost/my-db?host=$(realpath ./testdir/pg)/socket" ] || fail
+echo '~~ get-connstr'
+result=$(lpg on ./testdir/pg get-connstr) ; [ "$?" = 0 ] || fail
+[ "$result" = "postgresql://postgres@localhost/?host=$(realpath ./testdir/pg)/socket" ] || fail
+echo '~~ up'
+lpg on ./testdir/pg up ; [ "$?" = 0 ] || fail
+echo '~~ up'
+lpg on ./testdir/pg up ; [ "$?" = 0 ] || fail
+echo '~~ down'
+lpg on ./testdir/pg down ; [ "$?" = 0 ] || fail
+echo '~~ down'
+lpg on ./testdir/pg down ; [ "$?" = 0 ] || fail
 echo '~~ restart'
-lpg pg-restart ./testdir/pg ; echo "$?"
-echo '~~ stop'
-lpg pg-stop ./testdir/pg ; echo "$?"
-echo '~~ up'
-lpg pg-up ./testdir/pg ; echo "$?"
-echo '~~ up'
-lpg pg-up ./testdir/pg ; echo "$?"
-echo '~~ down'
-lpg pg-down ./testdir/pg ; echo "$?"
-echo '~~ down'
-lpg pg-down ./testdir/pg ; echo "$?"
+lpg on ./testdir/pg restart ; [ "$?" = 0 ] || fail
 echo '~~ echo LPG_LOC'
-out=$(lpg bash ./testdir/pg 'echo $LPG_LOC')
+out=$(lpg on ./testdir/pg run 'echo $LPG_LOC')
 echo "$out"
 [ "$out" = "$(realpath ./testdir/pg)" ] || fail
